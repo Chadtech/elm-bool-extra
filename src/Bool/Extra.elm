3ae169f22e990b073a49df88138a6fdb1d661313
Chadtech <chadstearns1@gmail.com>
2017-08-13 21:52:30 -0400

    Added real content to Elm file

--- src/Bool/Extra.elm
+++ src/Bool/Extra.elm
@@ -1,3 +1,57 @@
 module Bool.Extra
     exposing
-        (..)
+        ( all
+        , none
+        , any
+        , notAll
+        )
+
+
+{-| Convenience functions for working with Bools
+
+# Basics
+
+@docs all, none, some, notAll
+
+-}
+
+{-| All the bools are true.
+
+    all [ True, True ] == True
+    all [ True, False ] == False
+    all [ False, False ] == False
+-}
+all : List Bool -> Bool
+all =
+    List.all identity
+
+{-| None of the bools are true.
+
+    none [ True, True ] == False
+    none [ True, False ] == False
+    none [ False, False ] == True
+-}
+none : List Bool -> Bool
+none =
+    List.all (identity >> not)
+
+{-| At least one of the bools is true.
+
+    any [ True, True ] == True
+    any [ True, False ] == True
+    any [ False, False ] == False
+-}
+any : List Bool -> Bool
+any =
+    List.any identity
+
+{-| Not all of them are true
+
+    notAll [ True, True ] == False
+    notAll [ True, False ] == True
+    notAll [ False, False ] == True    
+-}
+notAll : List Bool -> Bool
+notAll =
+    all >> not
+
